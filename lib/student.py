b23e577d54b70e12332282c724a3f69283dcb34d
gerrad <gerrad.meitamei@student.moringaschool.com>
2025-05-19 11:58:54 +0300

    initial commit

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,24 @@
-class Student:
-    pass
+from lib.user import User
+
+class Student(User):
+    def __init__(self, first_name="First", last_name="Last"):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+    def learn(self, info):
+        self.knowledge.append(info)
+
+    def hello(self):
+        print("Hey there! I'm so excited to learn stuff.")
+
+    def raise_hand(self):
+        print("Pick me!")
+
 
 class ChattyStudent(Student):
-    pass
+    def hello(self):
+        print("Hey there! I'm so excited to learn stuff. Did you watch that show last night? So sad about who died...")
+
+    def raise_hand(self):
+        for _ in range(10):
+            print("Pick me!")
